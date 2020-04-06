01434e026c3a30551e89c94963f258304c550e09
Noam Zimran <noamzim12@gmail.com>
2020-04-07 01:19:22 +0300

    lab2 update 6.4.20

--- lab2_main.py
+++ lab2_main.py
@@ -7,6 +7,9 @@ from SingleImage import SingleImage
 import MatrixMethods
 from ObjectsSynthetic import *
 
+# מעבדה 2
+# שלב 1
+# חלק א'
 # create synthetic data
 edgeSize = 10   # [m]
 squere = np.array([[-edgeSize/2,edgeSize/2,0],[edgeSize/2,edgeSize/2,0],[edgeSize/2,-edgeSize/2,0],[-edgeSize/2,-edgeSize/2,0]])
@@ -18,7 +21,7 @@ camera1 = Camera(focal_length, np.array([0, 0]), None, None, None, sensor_size)
 
 # define image
 omega = 0
-phi = np.radians(50)
+phi = 0
 kappa = 0
 Z = 20   # [m]
 img1 = SingleImage(camera1)
@@ -28,47 +31,119 @@ img1.exteriorOrientationParameters = np.array([[0, 0, Z, omega, phi, kappa]])
 # # draw
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-pv.drawOrientation(img1.RotationMatrix,img1.PerspectiveCenter,1,ax)
-pv.drawImageFrame(sensor_size/1000,sensor_size/1000,img1.RotationMatrix,img1.PerspectiveCenter,camera1.focalLength/1000,10,ax)
-# # img1.drawSingleImage(squere,100,ax)
-# ax.scatter(squere[:,0],squere[:,1],squere[:,2], c='b', s=50,marker='^')
-# plt.show()
-#
-# imagePoints1 = img1.GroundToImage(squere)
-# groundPoints = img1.ImageToGround_GivenZ(imagePoints1,np.zeros(imagePoints1.shape[0]))
-# print(groundPoints)
+# pv.drawOrientation(img1.RotationMatrix,img1.PerspectiveCenter,1,ax)
+# pv.drawImageFrame(sensor_size/1000,sensor_size/1000,img1.RotationMatrix,img1.PerspectiveCenter,camera1.focalLength/1000,10,ax)
+img1.drawSingleImage(squere,100,ax,'yes')
+ax.scatter(squere[:,0],squere[:,1],squere[:,2], c='b', s=50,marker='^')
 
+
+imagePoints1 = img1.GroundToImage(squere)
+print(imagePoints1)
+
+img1.exteriorOrientationParameters, sigma0, sigmaX =  img1.ComputeExteriorOrientation(imagePoints1,squere,0.001)
+print('Adjustment test','\n',img1.exteriorOrientationParameters)
+
+# adding noise
+noiseSize = 0.01
+noise = np.random.normal(0, noiseSize, imagePoints1.shape)
+imagePoints1 += noise
+img1.exteriorOrientationParameters, sigma0, sigmaX =  img1.ComputeExteriorOrientation(imagePoints1,squere,0.001)
+print('after adding noise','\n',img1.exteriorOrientationParameters)
+
+# חלק ב'
+# cancel the noise
+img1.exteriorOrientationParameters = np.array([[0, 0, 20, 0, 0, 0]])
+
+# define synthetic image
 imgS1 = SingleImage(camera1,'synthetic')
 azimuth = np.radians(0)
 phi = np.radians(45)
 kappa = np.radians(0)
-Z = 100
+Z = 20
 imgS1.exteriorOrientationParameters = np.array([[0, 0, Z, azimuth, phi, kappa]])
 imgS1.exteriorOrientationParameters[0:3] = np.dot(imgS1.RotationMatrix,imgS1.PerspectiveCenter)
 
-imgS2 = SingleImage(camera1,'synthetic')
-imgS2.innerOrientationParameters = np.array([0,1,0,0,0,1])
-azimuth = np.radians(90)
-phi = np.radians(45)
-kappa = np.radians(45)
-Z = 100
-imgS2.exteriorOrientationParameters = np.array([[0, 0, Z, azimuth, phi, kappa]])
-imgS2.exteriorOrientationParameters[0:3] = np.dot(imgS2.RotationMatrix,imgS2.PerspectiveCenter)
-print(imgS2.PerspectiveCenter)
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+fig2 = plt.figure()
+ax = fig2.add_subplot(111, projection='3d')
+img1.drawSingleImage(squere,100,ax,'yes')
 imgS1.drawSingleImage(squere,100,ax,'yes')
-imgS2.drawSingleImage(squere,100,ax,'yes')
 ax.scatter(squere[:,0],squere[:,1],squere[:,2], c='b', s=50,marker='^')
 
-imagePoints_S1 = imgS1.GroundToImage(squere)
-imagePoints_S2 = imgS2.GroundToImage(squere)
-plt.figure()
-pv.drawImageFrame2D(imgS1.camera.sensorSize, imgS1.camera.sensorSize)
-plt.scatter(imagePoints_S1[:, 0], imagePoints_S1[:, 1])
-plt.figure()
-pv.drawImageFrame2D(imgS2.camera.sensorSize, imgS2.camera.sensorSize)
-plt.scatter(imagePoints_S2[:, 0], imagePoints_S2[:, 1])
+imagePoints2 = imgS1.GroundToImage(squere)
+print(imagePoints2)
 
-print(imgS2.ComputeExteriorOrientation(imagePoints_S2,squere,0.001))
+imgS1.innerOrientationParameters = np.array([0,1,0,0,0,1])
+imgS1.exteriorOrientationParameters, sigma0, sigmaX =  imgS1.ComputeExteriorOrientation(imagePoints2,squere,0.001)
+print(imgS1.exteriorOrientationParameters)
+
+# adding noise
+noiseSize = 0.01
+noise = np.random.normal(0, noiseSize, imagePoints2.shape)
+imagePoints2 += noise
+imgS1.exteriorOrientationParameters, sigma0, sigmaX =  imgS1.ComputeExteriorOrientation(imagePoints2,squere,0.001)
+print(imgS1.exteriorOrientationParameters)
+
+
+
+# שלב 2
+# define 8 synthetic image
+
+azimuth = np.radians(np.array([0,45,90,135,180,225,270,315]))
+phi = np.radians(45)
+kappa = np.radians(np.array([0,0,0,0,0,0,0,0]))
+Z = 20
+fig3 = plt.figure()
+ax = fig3.add_subplot(111, projection='3d')
+ax.scatter(squere[:,0],squere[:,1],squere[:,2], c='b', s=50,marker='^')
+img1.drawSingleImage(squere,100,ax,'yes')
+
+for i in range(len(azimuth)):
+    # define image
+    imgS = SingleImage(camera1,'synthetic')
+    imgS.exteriorOrientationParameters = np.array([[0, 0, Z, azimuth[i], phi, kappa[i]]])
+    imgS.exteriorOrientationParameters[0:3] = np.dot(imgS.RotationMatrix,imgS.PerspectiveCenter)
+    # draw image
+    imgS.drawSingleImage(squere,100,ax,'yes')
+
+    # imagePoints3 = imgS.GroundToImage(squere)
+    # print(imagePoints3)
+    #
+    # imgS.innerOrientationParameters = np.array([0,1,0,0,0,1])
+    # imgS.exteriorOrientationParameters, sigma0, sigmaX = imgS.ComputeExteriorOrientation(imagePoints3,squere,0.001)
+    # print(imgS.exteriorOrientationParameters)
+    #
+
+
+
 plt.show()
+
+
+
+
+
+# imgS2 = SingleImage(camera1,'synthetic')
+# imgS2.innerOrientationParameters = np.array([0,1,0,0,0,1])
+# azimuth = np.radians(90)
+# phi = np.radians(45)
+# kappa = np.radians(45)
+# Z = 100
+# imgS2.exteriorOrientationParameters = np.array([[0, 0, Z, azimuth, phi, kappa]])
+# imgS2.exteriorOrientationParameters[0:3] = np.dot(imgS2.RotationMatrix,imgS2.PerspectiveCenter)
+# print(imgS2.PerspectiveCenter)
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# imgS1.drawSingleImage(squere,100,ax,'yes')
+# imgS2.drawSingleImage(squere,100,ax,'yes')
+# ax.scatter(squere[:,0],squere[:,1],squere[:,2], c='b', s=50,marker='^')
+#
+# imagePoints_S1 = imgS1.GroundToImage(squere)
+# imagePoints_S2 = imgS2.GroundToImage(squere)
+# plt.figure()
+# pv.drawImageFrame2D(imgS1.camera.sensorSize, imgS1.camera.sensorSize)
+# plt.scatter(imagePoints_S1[:, 0], imagePoints_S1[:, 1])
+# plt.figure()
+# pv.drawImageFrame2D(imgS2.camera.sensorSize, imgS2.camera.sensorSize)
+# plt.scatter(imagePoints_S2[:, 0], imagePoints_S2[:, 1])
+#
+# print('^^^^^^^','\n',imgS2.ComputeExteriorOrientation(imagePoints_S2,squere,0.001))
+
